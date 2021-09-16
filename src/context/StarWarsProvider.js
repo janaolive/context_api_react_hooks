efdfd276b73511f2494942aaa6b3aca4ad82ff1a
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-09-16 19:41:55 -0300

    requisito 1

---
+++ src/context/StarWarsProvider.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import StarWarsContext from './StarWarsContext';
+
+function StarWarsProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  const fetchPlanets = () => (
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then((results) => setData([...results.results]))
+  );
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  const contextValue = {
+    data,
+  };
+
+  return (
+    <StarWarsContext.Provider value={ contextValue }>
+      { children }
+    </StarWarsContext.Provider>
+  );
+}
+
+export default StarWarsProvider;
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
