7c117d2fa87044f5ef139f9d029450a618540cf0
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-10-08 21:29:02 -0300

    reiniciando o projeto porque apos refatoraçoes começou a apresentar erros que nao consegui solucionar.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 // requisto concluido com consulta a: https://pt-br.reactjs.org/docs/hooks-intro.html, dúvidas compartilhadas no Slack e repositorio GitHub.
 import React from 'react';
 import './App.css';
-import StarWarsProvider from './context/StarWarsProvider';
-import Table from './components/Table';
-import Filters from './components/Filters';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <StarWarsProvider>
-      <Filters />
-      <Table />
-    </StarWarsProvider>
-  );
-}
+function App() {}
 
 export default App;
--- src/context/StarWarsContext.js
+++
@@ -1,7 +0,0 @@
-// requisto concluido com consulta a: https://pt-br.reactjs.org/docs/hooks-intro.html, dúvidas compartilhadas no Slack e repositorio GitHub.
-
-import { createContext } from 'react';
-
-const StarWarsContext = createContext();
-
-export default StarWarsContext;
